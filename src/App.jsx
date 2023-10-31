31205c7b048fe1183071d872da011efa53f2eafc
Nathaly <118780236+nathalyconforme@users.noreply.github.com>
2023-10-31 21:48:34 +0000

    first commit on React portfolio from Microsoft Learn

--- src/App.jsx
+++ src/App.jsx
@@ -24,15 +24,14 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
+  name: "Nathaly Conforme",
+  title: "Frontend Developer",
+  email: "nathalyconformed.ncd@gmail.com",
+  gitHub: "nathalyconforme",
+  instagram: "nathaly_conforme",
+  linkedIn: "nathalyconforme",
   medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
+  twitter: "nacoDev",
 };
 
 const primaryColor = "#4E567E";
